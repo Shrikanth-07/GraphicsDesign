39dddfb7b12cbf6dc98c76c490f4a29b8bd73592
Shrikanth-07 <ugcet2502244@reva.edu.in>
2026-06-06 21:30:56 +0530

    Update graphic1.c
    Added line drawing feature

--- graphic1.c
+++ graphic1.c
@@ -34,6 +34,46 @@ void displayimage()
     }
 }
 
+void drawStraightline(int x1, int y1, int x2, int y2)
+{
+    int i;
+
+    if(x1 == x2)
+    {
+        if(y1 > y2)
+        {
+            int temp = y1;
+            y1 = y2;
+            y2 = temp;
+        }
+
+        for(i = y1; i <= y2; i++)
+        {
+            if(i >= 0 && i < ROWS && x1 >= 0 && x1 < COLS)
+                data[i][x1] = '*';
+        }
+    }
+    else if(y1 == y2)
+    {
+        if(x1 > x2)
+        {
+            int temp = x1;
+            x1 = x2;
+            x2 = temp;
+        }
+
+        for(i = x1; i <= x2; i++)
+        {
+            if(y1 >= 0 && y1 < ROWS && i >= 0 && i < COLS)
+                data[y1][i] = '*';
+        }
+    }
+    else
+    {
+        printf("Only horizontal and vertical lines are supported.\n");
+    }
+}
+
 int main()
 {
     int choice;
@@ -45,6 +85,7 @@ int main()
         printf("\n===== 2D GRAPHICS EDITOR =====\n");
         printf("0. Exit\n");
         printf("1. Display Image\n");
+        printf("2.Draw Line\n");
         printf("Enter Choice: ");
         scanf("%d", &choice);
 
@@ -57,6 +98,15 @@ int main()
             case 1:
                 displayimage();
                 break;
+            case 2:
+            {
+                int x1,y1,x2,y2;
+                printf("Enter x1 y1 x2 y2: ");
+                scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
+
+                drawStraightline(x1,y1,x2,y2);
+                break;
+            }    
 
             default:
                 printf("Invalid Choice\n");
