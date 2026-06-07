acbf092eba9203cecb4ab88f149cf59676a818aa
Shrikanth-07 <ugcet2502244@reva.edu.in>
2026-06-07 20:15:39 +0530

    Update graphic1.c
    added Rectangle and Triangle

--- graphic1.c
+++ graphic1.c
@@ -74,6 +74,58 @@ void drawStraightline(int x1, int y1, int x2, int y2)
     }
 }
 
+void drawRectangle(int x, int y, int width, int height)
+{
+    int i;
+
+    for(i = x; i < x + width; i++)
+    {
+        if(y >= 0 && y < ROWS && i >= 0 && i < COLS)
+            data[y][i] = '*';
+
+        if(y + height - 1 >= 0 && y + height - 1 < ROWS &&
+           i >= 0 && i < COLS)
+            data[y + height - 1][i] = '*';
+    }
+
+    for(i = y; i < y + height; i++)
+    {
+        if(i >= 0 && i < ROWS && x >= 0 && x < COLS)
+            data[i][x] = '*';
+
+        if(i >= 0 && i < ROWS &&
+           x + width - 1 >= 0 &&
+           x + width - 1 < COLS)
+        {
+            data[i][x + width - 1] = '*';
+        }
+    }
+}
+
+void drawTriangle(int row, int col, int height)
+{
+    int i, j;
+
+    for(i = 0; i < height; i++)
+    {
+        for(j = col - i; j <= col + i; j++)
+        {
+            if(i == height - 1 ||
+               j == col - i ||
+               j == col + i)
+            {
+                if(row + i >= 0 &&
+                   row + i < ROWS &&
+                   j >= 0 &&
+                   j < COLS)
+                {
+                    data[row + i][j] = '*';
+                }
+            }
+        }
+    }
+}
+
 int main()
 {
     int choice;
@@ -86,6 +138,8 @@ int main()
         printf("0. Exit\n");
         printf("1. Display Image\n");
         printf("2.Draw Line\n");
+        printf("3.Draw Rectangle\n"); 
+        printf("4.Draw Triangle\n"); 
         printf("Enter Choice: ");
         scanf("%d", &choice);
 
@@ -105,6 +159,26 @@ int main()
                 scanf("%d%d%d%d",&x1,&y1,&x2,&y2);
 
                 drawStraightline(x1,y1,x2,y2);
+                break;
+            }
+            case 3:
+            {
+                int x, y, width, height;
+
+                printf("Enter x y width height: ");
+                scanf("%d%d%d%d", &x, &y, &width, &height);
+
+                drawRectangle(x, y, width, height);
+                break;
+            }
+            case 4:
+            {
+                 int row, col, height;
+
+                printf("Enter row column height: ");
+                scanf("%d%d%d", &row, &col, &height);
+
+                drawTriangle(row, col, height);
                 break;
             }    
 
